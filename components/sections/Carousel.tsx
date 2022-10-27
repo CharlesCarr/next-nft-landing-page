241bbb647dd83a376517021802d38a2349e8025e
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-10-27 15:01:08 -0400

    add positioning and components to carousel section

--- components/sections/Carousel.tsx
+++ components/sections/Carousel.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Carousel = () => {
   return (
-    <div className="bg-slate-600 w-full h-60 text-center text-white">TOP NFT CAROUSEL</div>
-  )
-}
+    <div className="bg-slate-600 w-full h-[100vh] flex justify-center items-center relative overflow-hidden text-white">
+      <div className="w-3/4 mx-0 my-auto flex justify-center items-center">
+        <div className="w-1/2 h-full min-h-[60vh] flex flex-col items-center justify-center">
+          <p>Carousel</p>
+        </div>
 
-export default Carousel
+        <div className="w-3/4 mx-0 my-auto flex flex-col justify-center items-center">
+          <h2>Welcome to the club....</h2>
+          <p>Description....</p>
+          <p>More description...</p>
+
+          <button className="bg-slate-200 text-black rounded-2xl px-4 py-2">Join Our Discord</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
