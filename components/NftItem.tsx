7997937e39b82b6a2f0d7a234b7818299433bd40
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-10-31 14:50:23 -0400

    fix: update showcase components

--- components/NftItem.tsx
+++ components/NftItem.tsx
@@ -12,17 +12,17 @@ const NftItem = ({ img = 0, number = 0, price = 0, passRef }: any) => {
 
   return (
     <div
-      className="w-52 h-14 mx-4 my-4 cursor-pointer border border-black rounded-full flex text-xs"
+      className="w-52 h-16 mx-4 my-4 cursor-pointer border border-black rounded-full text-xs flex items-center justify-center px-4"
       onMouseOver={(e) => pause(e)}
       onMouseOut={(e) => play(e)}
     >
-      <div className="h-full w-1/4">
-        <p className="">Icon</p>
+      <div className="h-full w-1/4 flex justify-center items-center">
+        <p className="border border-black rounded-full h-10 w-10 flex justify-center items-center">Icon</p>
       </div>
 
-      <div className="h-full w-3/4">
-        <p>Creator Name</p>
-        <p>8,237+ Items</p>
+      <div className="h-full w-3/4 flex flex-col justify-center items-center">
+        <p className="text-sm font-bold">Creator Name</p>
+        <p className="font-light">8,237+ Items</p>
       </div>
     </div>
   );
