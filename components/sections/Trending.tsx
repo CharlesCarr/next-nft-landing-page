9c81f3cca3effbeb82dd2e43f7d621e6b34a0968
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-01 10:54:36 -0400

    feat: add responsiveness to trending section

--- components/sections/Trending.tsx
+++ components/sections/Trending.tsx
@@ -36,27 +36,22 @@ const Trending = () => {
       <div className="w-screen h-1/2 bg-black"></div>
       <div className="w-screen h-1/2 bg-white"></div>
       <div className="w-3/4 h-5/6 absolute m-auto top-12 right-0 left-0 bottom-0 grid grid-rows-4 grid-cols-5 gap-3">
-        <div className="row-start-1 row-end-2 col-start-1 col-end-4 flex justify-start items-start text-white text-3xl font-bold tracking-wider">
-          <p>
+        <div className="row-start-1 row-end-2 col-start-1 col-end-4 flex justify-start items-start text-white text-2xl sm:text-3xl font-bold tracking-wider">
+          <p className="w-40 md:w-full">
             Trending Artworks <br /> This Week
           </p>
         </div>
 
-        {/* {trendingNFTs.map((nft: any) => {
-          <div className="row-start-2 row-end-3 col-start-1 col-end-2 border border-black">
-            <NFTCard {...nft} />
-          </div>
-        })} */}
-        <div className="bg-slate-100 row-start-2 row-end-4 col-start-1 col-end-2 border border-black">
+        <div className="bg-slate-100 row-start-2 row-end-4 col-start-1 col-end-3 md:col-end-2 border border-black">
           <NFTCard {...trendingNFTs[0]} />
         </div>
-        <div className="bg-slate-100 row-start-2 row-end-4 col-start-2 col-end-3 border border-black">
+        <div className="bg-slate-100 row-start-2 row-end-4 col-start-2 col-end-3 border border-black hidden md:flex">
           <NFTCard {...trendingNFTs[1]} />
         </div>
-        <div className="bg-slate-100 row-start-2 row-end-4 col-start-3 col-end-4 border border-black">
+        <div className="bg-slate-100 row-start-2 row-end-4 col-start-3 col-end-4 border border-black hidden md:flex">
           <NFTCard {...trendingNFTs[2]} />
         </div>
-        <div className="bg-slate-100 row-start-1 row-end-4 col-start-4 col-end-6 flex justify-center items-center border border-black">
+        <div className="bg-slate-100 row-start-1 row-end-4 col-start-3 md:col-start-4 col-end-6 flex justify-center items-center border border-black">
           <NFTCard nft="NFT 4" name="@charlie" price="2 ETH" totalhw="trs-total" tbheight="trs-tb" imgheight="trs-img" background={false} />
         </div>
       </div>
