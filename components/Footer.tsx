bd22d79b842a6ada6e69b82621921e073d7e8fb4
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-01 23:36:37 -0400

    feat: add social icons to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,45 @@
+import {
+  BsFacebook,
+  BsTwitter,
+  BsTwitch,
+  BsLinkedin,
+  BsYoutube,
+  BsDiscord,
+  BsGithub,
+} from "react-icons/bs";
+
 const Footer = () => {
+  const icons = [
+    { icon: <BsFacebook className="w-4 h-4" /> },
+    { icon: <BsTwitter className="w-4 h-4" /> },
+    { icon: <BsTwitch className="w-4 h-4" /> },
+    { icon: <BsLinkedin className="w-4 h-4" /> },
+    { icon: <BsYoutube className="w-4 h-4" /> },
+    { icon: <BsDiscord className="w-4 h-4" /> },
+    { icon: <BsGithub className="w-4 h-4" /> },
+  ];
+
   return (
-    <div className="h-fit w-screen flex flex-col items-center justify-end" id="community">
+    <div
+      className="h-fit w-screen flex flex-col items-center justify-end"
+      id="community"
+    >
       <div className="w-full h-44 bg-black flex justify-between items-center px-8 sm:px-32">
-        <p className="text-xl sm:text-3xl font-bold text-white">Join Our Community</p>
-         <button>
-            <a
-              href="#_"
-              className="relative inline-block px-4 py-2 font-medium group text-xs sm:text-base"
-            >
-              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-2 translate-y-2 bg-black border border-white group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-              <span className="absolute inset-0 w-full h-full bg-white border-2 border-white group-hover:bg-black group-hover:border-white"></span>
-              <span className="relative text-black group-hover:text-white">
-                Launch Discord
-              </span>
-            </a>
-          </button>
+        <p className="text-xl sm:text-3xl font-bold text-white">
+          Join Our Community
+        </p>
+        <button>
+          <a
+            href="#_"
+            className="relative inline-block px-4 py-2 font-medium group text-xs sm:text-base"
+          >
+            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-2 translate-y-2 bg-black border border-white group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span className="absolute inset-0 w-full h-full bg-white border-2 border-white group-hover:bg-black group-hover:border-white"></span>
+            <span className="relative text-black group-hover:text-white">
+              Launch Discord
+            </span>
+          </a>
+        </button>
       </div>
       <div className="w-full h-fit bg-white flex justify-between items-center pl-6 sm:pl-12 md:pl-32 mt-10 pb-5 sm:pb-20">
         <div className="w-1/4 h-full">
@@ -26,22 +51,25 @@ const Footer = () => {
             collectibles and non-fungible tokens (NFTs). Buy, sell, and discover
             exclusive digital items.
           </p>
-          <ul className="flex p-0 m-0 text-xs justify-start items-center">
-            <li className="border border-black rounded-full h-8 w-8 mr-2 flex justify-center items-center">
-              fb
-            </li>
-            <li className="border border-black rounded-full h-8 w-8 mr-2 flex justify-center items-center">
-              li
-            </li>
-            <li className="border border-black rounded-full h-8 w-8 flex justify-center items-center">
-              tw
-            </li>
+          <ul className="flex p-0 m-0 text-xs justify-start items-center w-full">
+            {icons.map((i: any, index: number) => {
+              return (
+                <li
+                  key={index}
+                  className="mr-2 sm:mr-3 flex justify-center items-center"
+                >
+                  {i.icon}
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className="flex w-2/4 h-full pr-8 sm:pr-16">
           <div className="w-1/4 mr-12 sm:mr-0">
             <ul className="m-0 p-0">
-              <li className="font-bold mb-6 text-xs sm:text-sm sm:tracking-wider">Marketplace</li>
+              <li className="font-bold mb-6 text-xs sm:text-sm sm:tracking-wider">
+                Marketplace
+              </li>
               <li className="font-light text-xs mb-4">All NFTs</li>
               <li className="font-light text-xs mb-4">Collectibles</li>
               <li className="font-light text-xs mb-4">Photography</li>
@@ -50,14 +78,18 @@ const Footer = () => {
           </div>
           <div className="w-1/4 hidden sm:flex">
             <ul className="m-0 p-0">
-              <li className="font-bold mb-6 text-xs sm:text-sm sm:tracking-wider">Statistics</li>
+              <li className="font-bold mb-6 text-xs sm:text-sm sm:tracking-wider">
+                Statistics
+              </li>
               <li className="font-light text-xs mb-4">Rankings</li>
               <li className="font-light text-xs mb-4">Activity</li>
             </ul>
           </div>
           <div className="w-1/4 hidden sm:flex">
             <ul className="m-0 p-0">
-              <li className="font-bold mb-6 text-xs sm:text-sm sm:tracking-wider">Resources</li>
+              <li className="font-bold mb-6 text-xs sm:text-sm sm:tracking-wider">
+                Resources
+              </li>
               <li className="font-light text-xs mb-4">Help Center</li>
               <li className="font-light text-xs mb-4">Partners</li>
               <li className="font-light text-xs mb-4">Docs</li>
@@ -66,7 +98,9 @@ const Footer = () => {
           </div>
           <div className="w-1/4">
             <ul className="m-0 p-0">
-              <li className="font-bold mb-6 text-xs sm:text-sm sm:tracking-wider">Company</li>
+              <li className="font-bold mb-6 text-xs sm:text-sm sm:tracking-wider">
+                Company
+              </li>
               <li className="font-light text-xs mb-4">About Us</li>
               <li className="font-light text-xs mb-4">Careers</li>
               <li className="font-light text-xs mb-4">Contact Us</li>
